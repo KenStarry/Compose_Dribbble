69e6d99ee703f7ae882d5dcced49df9651583861
KenStarry <starrycodes@gmail.com>
2023-06-20 15:40:03 +0300

    Finished payment section

--- Ecommy/app/src/main/java/com/kenstarry/ecommy/features/feature_checkout/presentation/components/PaymentSection.kt
+++ Ecommy/app/src/main/java/com/kenstarry/ecommy/features/feature_checkout/presentation/components/PaymentSection.kt
@@ -1,29 +1,45 @@
 package com.kenstarry.ecommy.features.feature_checkout.presentation.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kenstarry.ecommy.R
+import com.kenstarry.ecommy.ui.theme.Pink80
 
 @Composable
 fun PaymentSection() {
+
+    val logos = listOf(
+        R.drawable.addidas_logo_pngwing,
+        R.drawable.jordans_logo_pngwing,
+        R.drawable.nike_logo_pngwing,
+        R.drawable.puma_logo_pngwing
+    )
 
     Column(
         modifier = Modifier
@@ -54,18 +70,32 @@ fun PaymentSection() {
         //  payment methods
         LazyRow(
             state = rememberLazyListState(),
+            horizontalArrangement = Arrangement.spacedBy(24.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth(),
             content = {
-                items(10) { method ->
-
+                itemsIndexed(logos) { index, logo ->
                     Box(
                         modifier = Modifier
                             .size(60.dp)
-                            .clip(RoundedCornerShape(16)),
+                            .clip(RoundedCornerShape(20))
+                            .border(
+                                width = 3.dp,
+                                color = Pink80.copy(alpha = 0.5f),
+                                shape = RoundedCornerShape(20)
+                            ),
                         contentAlignment = Alignment.Center
                     ) {
-
+                        Image(
+                            painter = painterResource(id = logo),
+                            contentDescription = "Addidas logo",
+                            modifier = Modifier
+                                .size(45.dp)
+                        )
                     }
                 }
+
             })
 
     }
