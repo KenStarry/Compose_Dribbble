545395f6952451d9ad16e1adb19e4dddcc8595c1
KenStarry <starrycodes@gmail.com>
2023-06-20 15:53:58 +0300

    Added total section

---
+++ Ecommy/app/src/main/java/com/kenstarry/ecommy/features/feature_checkout/presentation/components/TotalSection.kt
@@ -0,0 +1,24 @@
+package com.kenstarry.ecommy.features.feature_checkout.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TotalSection() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+
+
+    }
+}
